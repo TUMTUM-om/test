65bf420f8fc32a607dd659320b03c1b44f5dbe64
Harsh.7k <harshkaware408@gmail.com>
2025-04-29 23:47:47 +0530

    Update addition.java

--- addition.java
+++ addition.java
@@ -1,6 +1,6 @@
 public class addition {
     public static void main(String[] args) {
-        int a = 20, b = 30;
+        int a = 200, b = 30;
         int sum = a + b;
         System.out.println("Sum: " + sum);
     }
